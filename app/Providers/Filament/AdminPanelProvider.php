0ce46f08bdeefd72f0d8a5d79b82b8b8b81d8561
arshadmultani <arshadrmultani@gmail.com>
2025-06-03 17:14:25 +0530

    feat:add filament-approvals package(3.x-dev)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -78,9 +78,24 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->plugins([
-                FilamentShieldPlugin::make(),
+                FilamentShieldPlugin::make()->gridColumns([
+                    'default' => 1,
+                    'sm' => 2,
+                    'lg' => 3
+                ])
+                ->sectionColumnSpan(1)
+                ->checkboxListColumns([
+                    'default' => 1,
+                    'sm' => 2,
+                    'lg' => 4,
+                ])
+                ->resourceCheckboxListColumns([
+                    'default' => 1,
+                    'sm' => 2,
+                ]),
                 SimpleLightBoxPlugin::make(),
-                \RickDBCN\FilamentEmail\FilamentEmail::make()
+                \RickDBCN\FilamentEmail\FilamentEmail::make(),
+                \EightyNine\Approvals\ApprovalPlugin::make(),
             ])
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
             ->databaseNotifications()
