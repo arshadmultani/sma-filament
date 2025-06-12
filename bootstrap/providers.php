36447e703ca03d500525d43e465acb637eeb23f9
arshadmultani <arshadrmultani@gmail.com>
2025-06-12 19:47:21 +0530

    revert: remove vibe coded pwaServiceProvider and moving to pwa-generator package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\FilamentPwaServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\FilamentPwaServiceProvider::class,
 ];
