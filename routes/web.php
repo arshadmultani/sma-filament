2a153a86f23b6f46ad3e4f96b109b936e2118825
arshadmultani <arshadrmultani@gmail.com>
2025-06-13 10:49:39 +0530

    revert:commenting welcome page for simplicity and routing errors

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    if (Auth::check()) {
-        // Redirect to Filament dashboard (adjust route name if needed)
-        return redirect()->route('filament.admin.pages.dashboard');
-    }
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     if (Auth::check()) {
+//         // Redirect to Filament dashboard (adjust route name if needed)
+//         return redirect()->route('filament.admin.pages.dashboard');
+//     }
+//     return view('welcome');
+// });
 
