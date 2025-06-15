c55028605ec6fa72c9a7ebc5cf218ab3753fcb9d
arshadmultani <arshadrmultani@gmail.com>
2025-06-15 20:10:37 +0530

    feat:add pwa manifest in welocme page head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,22 @@ $quote = str_replace('“', '', $quote);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome | Charak SMA</title>
+    <title>Welcome | {{ config('app.name') }}</title>
+    <!-- PWA Manifest -->
+    <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#000000">
+        <meta name="background-color" content="#ffffff">
+        <!-- PWA Icons (optional, for iOS support) -->
+        <link rel="apple-touch-icon" sizes="72x72" href="/images/icons/icon.png">
+
+        <!-- Register Service Worker -->
+        <script>
+            if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                    navigator.serviceWorker.register('/pwabuilder-sw.js');
+                });
+            }
+        </script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
     <style>
         html, body {
