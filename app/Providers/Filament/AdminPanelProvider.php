0e447f81ae171c8e6f23f1489c0c225077d77a28
arshadmultani <arshadrmultani@gmail.com>
2025-06-15 20:37:00 +0530

    feat:path set to admin for panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('admin')
             ->login(CustomLogin::class)
             ->darkMode(false)
             ->defaultThemeMode(ThemeMode::Light)
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     if (Auth::check()) {
-        // Redirect to Filament dashboard (adjust route name if needed)
         return redirect()->route('filament.admin.pages.dashboard');
     }
     return view('welcome');
