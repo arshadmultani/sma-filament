d8a983e77bd507655d8787d25094a76b29f3f6ab
arshadmultani <arshadrmultani@gmail.com>
2025-06-23 15:58:45 +0530

    feat:remove redundant files

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,7 +1,6 @@
 <x-filament-panels::page>
     @livewire('greeting')
     @livewire('heading', ['title' => 'Active Campaigns'])
-    @livewire('card')
     @livewire('campaign-card')
 </x-filament-panels::page>
 
